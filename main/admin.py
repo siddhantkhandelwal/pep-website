49d1a9166c0b3e060aafb027a505dee36db379bc
siddhantkhandelwal <siddhant@outlook.com>
2018-09-20 00:10:39 +0530

    Minor changes to Admin Page

--- main/admin.py
+++ main/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from .models import Paper, ProfessorProfile, ParticipantProfile, Abstract, Category, College
 
 
+class ParticipantProfileAdmin(admin.ModelAdmin):
+	list_display = ('author1', 'author2', 'college', 'phone1', 'no_of_abstracts')
+	def no_of_abstracts(self, obj):
+		return Abstract.objects.filter(participant = obj).count()
+
+class ProfessorProfileAdmin(admin.ModelAdmin):
+	list_display = ('display_name', 'category', 'phone1')
+
+
 admin.site.register(Category)
 admin.site.register(Paper)
 admin.site.register(Abstract)
-admin.site.register(ParticipantProfile)
-admin.site.register(ProfessorProfile)
+admin.site.register(ParticipantProfile, ParticipantProfileAdmin)
+admin.site.register(ProfessorProfile, ProfessorProfileAdmin)
 admin.site.register(College)
