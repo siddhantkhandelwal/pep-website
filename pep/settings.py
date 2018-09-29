e8cf2561f37132603421f406d97df2a7a4a4fbc5
siddhantkhandelwal <siddhant@outlook.com>
2018-09-29 19:48:36 +0530

    Minor changes

--- pep/settings.py
+++ pep/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = '/pep-static/'
+MEDIA_URL = '/pep-media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
