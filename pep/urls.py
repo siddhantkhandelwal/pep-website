941414228ee21288dd5054ea50ee26d724b9cef1
siddhantkhandelwal <siddhant@outlook.com>
2018-09-29 18:44:57 +0530

    Minor changes

--- pep/urls.py
+++ pep/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^main/', include('main.urls')),
+    url(r'^', include('main.urls')),
 ]
 
 if settings.DEBUG:
