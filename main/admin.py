33a09747cfa9df8e4d835bb7bba6200ae6d6e4a1
Siddhant Khandelwal <siddhant@outlook.com>
2019-02-07 18:36:25 +0530

    Admin Paper Changes

--- main/admin.py
+++ main/admin.py
@@ -3,101 +3,112 @@ from .models import Paper, ProfessorProfile, ParticipantProfile, Abstract, Categ
 
 
 class ParticipantProfileAdmin(admin.ModelAdmin):
-	list_display = ('author', 'coauthor', 'phone1', 'phone2', 'no_of_abstracts', 'college_name',)
-	
-	def no_of_abstracts(self, obj):
-		return Abstract.objects.filter(participant = obj).count()
+    list_display = ('author', 'coauthor', 'phone1', 'phone2',
+                    'no_of_abstracts', 'college_name',)
 
-	def college_name(self, obj):
-		return obj.college.name
+    def no_of_abstracts(self, obj):
+        return Abstract.objects.filter(participant=obj).count()
+
+    def college_name(self, obj):
+        return obj.college.name
 
 
 class CollegeAdmin(admin.ModelAdmin):
-	list_display = ('name', 'no_of_abstracts',)
+    list_display = ('name', 'no_of_abstracts',)
 
-	def no_of_abstracts(self, obj):
-		return Abstract.objects.filter(participant__college = obj).count()
-		
+    def no_of_abstracts(self, obj):
+        return Abstract.objects.filter(participant__college=obj).count()
+
 
 class ProfessorProfileAdmin(admin.ModelAdmin):
-	list_display = ('display_name', 'category', 'phone1')
+    list_display = ('display_name', 'category', 'phone1')
 
 
 class SupervisorProfileAdmin(admin.ModelAdmin):
-	list_display = ('user', 'allotted_categories', 'staff_allotted')
+    list_display = ('user', 'allotted_categories', 'staff_allotted')
 
-	def staff_allotted(self, obj):
-		staff_allotted_list = []
-		for supervisor_category in obj.categories.all():
-			for staff in StaffProfile.objects.all():
-				for staff_category in staff.categories.all():
-					if supervisor_category == staff_category:
-						if staff not in staff_allotted_list:
-							staff_allotted_list.append(staff)
-		return staff_allotted_list
+    def staff_allotted(self, obj):
+        staff_allotted_list = []
+        for supervisor_category in obj.categories.all():
+            for staff in StaffProfile.objects.all():
+                for staff_category in staff.categories.all():
+                    if supervisor_category == staff_category:
+                        if staff not in staff_allotted_list:
+                            staff_allotted_list.append(staff)
+        return staff_allotted_list
 
-	def allotted_categories(self, obj):
-		return ', '.join([category.name for category in obj.categories.all()])
+    def allotted_categories(self, obj):
+        return ', '.join([category.name for category in obj.categories.all()])
 
 
 class StaffProfileAdmin(admin.ModelAdmin):
-	list_display = ('user', 'allotted_categories', 'no_of_abstracts')
-	
-	def allotted_categories(self, obj):
-		return ', '.join([category.name for category in obj.categories.all()])
+    list_display = ('user', 'allotted_categories', 'no_of_abstracts')
 
-	def no_of_abstracts(self, obj):
-		return Abstract.objects.filter(staff=obj).count()
+    def allotted_categories(self, obj):
+        return ', '.join([category.name for category in obj.categories.all()])
+
+    def no_of_abstracts(self, obj):
+        return Abstract.objects.filter(staff=obj).count()
 
 
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ('name', 'no_of_abstracts')
-	def no_of_abstracts(self, obj):
-		return Abstract.objects.filter(category=obj).count()
+    list_display = ('name', 'no_of_abstracts')
+
+    def no_of_abstracts(self, obj):
+        return Abstract.objects.filter(category=obj).count()
 
 
 class AbstractAdmin(admin.ModelAdmin):
-	list_display = ('uid', 'title', 'category', 'participant_author', 'participant_coauthor', 'professor_name', 'staff_handlers', 'status', 'verdict', 'college_name')
-	
-	def college_name(self, obj):
-		return obj.participant.college.name
+    list_display = ('uid', 'title', 'category', 'participant_author', 'participant_coauthor',
+                    'professor_name', 'staff_handlers', 'status', 'verdict', 'college_name')
 
-	def staff_handlers(self, obj):
-		return [staff.user.username for staff in obj.staff.all()]
+    def college_name(self, obj):
+        return obj.participant.college.name
 
-	def participant_author(self, obj):
-		return obj.participant.author
+    def staff_handlers(self, obj):
+        return [staff.user.username for staff in obj.staff.all()]
 
-	def participant_coauthor(self, obj):
-		return obj.participant.coauthor
+    def participant_author(self, obj):
+        return obj.participant.author
 
-	def professor_name(self, obj):
-		if obj.professor is not None:
-			return obj.professor.display_name
-		else:
-			return 'NA'
+    def participant_coauthor(self, obj):
+        return obj.participant.coauthor
+
+    def professor_name(self, obj):
+        if obj.professor is not None:
+            return obj.professor.display_name
+        else:
+            return 'NA'
 
 
 class PaperAdmin(admin.ModelAdmin):
-	list_display = ('uid', 'title', 'category', 'participant_author', 'participant_coauthor', 'professor_name', 'staff_handlers', 'status', 'verdict', 'college_name')
-	
-	def college_name(self, obj):
-		return obj.abstract.participant.college.name
+    list_display = ('abstract', 'title', 'document', 'category', 'participant_author',
+                    'participant_coauthor', 'professor_name', 'staff_handlers', 'status', 'verdict', 'college_name')
 
-	def staff_handlers(self, obj):
-		return [staff.user.username for staff in obj.abstract.staff.all()]
+    def title(self, obj):
+        return obj.abstract.title
 
-	def participant_author(self, obj):
-		return obj.abstract.participant.author
+    def category(self, obj):
+        return obj.abstract.category.name
 
-	def participant_coauthor(self, obj):
-		return obj.abstract.participant.coauthor
+    def college_name(self, obj):
+        return obj.abstract.participant.college.name
 
-	def professor_name(self, obj):
-		if obj.professor is not None:
-			return obj.abstract.professor.display_name
-		else:
-			return 'NA'
+    def staff_handlers(self, obj):
+        return [staff.user.username for staff in obj.abstract.staff.all()]
+
+    def participant_author(self, obj):
+        return obj.abstract.participant.author
+
+    def participant_coauthor(self, obj):
+        return obj.abstract.participant.coauthor
+
+    def professor_name(self, obj):
+        if obj.abstract.professor is not None:
+            return obj.abstract.professor.display_name
+        else:
+            return 'NA'
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Paper, PaperAdmin)
